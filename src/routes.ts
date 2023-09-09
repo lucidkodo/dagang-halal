7de2f5fd824941ef562a269715dc762e8acfe233
angie -r <lucidkodo@gmail.com>
2023-09-10 01:53:26 +0800

    1. Add beforeEnter hook for home page

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { RouteRecordRaw } from 'vue-router';
+import { useCurrentUserStore } from './store';
 import Login from './pages/Login.vue';
 import Organizations from './pages/organizations/index.vue';
 import OrganizationDetails from './pages/organizations/Details.vue';
@@ -7,6 +8,19 @@ export const OrgRoute: RouteRecordRaw = {
   path: '/',
   name: 'Organizations',
   component: Organizations,
+  /**
+   * Utilize the beforeEnter hook to check the store for sessions,
+   * and redirect users when there's no session.
+   */
+  beforeEnter: (to, from, next) => {
+    const store = useCurrentUserStore();
+    if (store.currentUser === null) {
+      next(LoginRoute);
+      return; // so next() doesn't get called twice
+    }
+
+    next();
+  },
 };
 
 export const OrgDetailsRoute: RouteRecordRaw = {
